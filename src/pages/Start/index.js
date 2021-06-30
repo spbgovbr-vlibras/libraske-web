c4e0e2e476319c78722afad65a6352bb7554d9cc
Arnor Neto <arnor.neto@lavid.ufpb.br>
2021-06-30 16:35:41 -0300

    add start page

---
+++ src/pages/Start/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import Button from "../../components/Button"
+
+const Start = () => {
+  const govAuth = () => {
+    window.location.href = `${process.env.REACT_APP_LOGIN_UNICO}/authorize?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&scope=openid+email+phone+profile&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
+  };
+
+  return (
+    <div id="wrapper">
+        <Button title={"Conta Gov.br"} onClick={govAuth}/>
+    </div>
+  );
+};
+
+export default Start;
