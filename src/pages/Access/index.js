5c89d07f32d02f105fd91e42949319874ba79cc8
Arnor Neto <arnor.neto@lavid.ufpb.br>
2021-07-07 10:29:25 -0300

    renamed 'start' route to 'access'

--- src/pages/Access/index.js
+++ src/pages/Access/index.js
@@ -1,16 +1,16 @@
 import "./styles.css";
 import Button from "../../components/Button"
 
-const Start = () => {
+const Access = () => {
   const govAuth = () => {
     window.location.href = `${process.env.REACT_APP_LOGIN_UNICO}/authorize?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&scope=openid+email+phone+profile&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
   };
 
   return (
-    <div id="page-start">
+    <div id="page-access">
         <Button title={"Conta Gov.br"} onClick={govAuth}/>
     </div>
   );
 };
 
-export default Start;
+export default Access;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 import { Switch, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
-import Start from "../pages/Start";
+import Access from "../pages/Access";
 import Authorize from "../pages/Authorize";
 
 const Routes = () => {
   return (
     <Switch>
       <Route component={Home} path="/" exact />
-      <Route component={Start} path="/start" />
+      <Route component={Access} path="/access" />
       <Route component={Authorize} path="/authorize" />
       {/* <Route component={CreatePoint} path="/create-point"/> */}
     </Switch>
