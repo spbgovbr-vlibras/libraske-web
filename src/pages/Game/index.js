d96143f926b7288d2b2062434f14df5e7b7ca83b
Arnor Neto <arnor.neto@lavid.ufpb.br>
2021-07-09 16:20:19 -0300

    add unity build to game page

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,10 +1,19 @@
 import "./styles.css";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import Unity, { UnityContext } from "react-unity-webgl";
 
 const Game = () => {
   const auth = useSelector((state) => state.auth);
   let history = useHistory();
+
+  const unityContext = new UnityContext({
+    loaderUrl: "unity/react.loader.js",
+    dataUrl: "unity/react.data",
+    frameworkUrl: "unity/react.framework.js",
+    codeUrl: "unity/react.wasm",
+  });
 
   const logout = () => {
     sessionStorage.clear();
@@ -17,9 +26,24 @@ const Game = () => {
     history.push("/");
   };
 
+  //Send information to Unity on load
+  useEffect(function () {
+    unityContext.on("loaded", function () {
+      setTimeout(() => {
+        unityContext.send("AccessSetup", "SetRefreshToken", auth.refresh_token);
+        unityContext.send("AccessSetup", "SetAccessToken", auth.access_token);
+        unityContext.send("AccessSetup", "SetName", auth.name);
+        unityContext.send("AccessSetup", "SetEmail", auth.email);
+      }, 100);
+    });
+  }, []);
+
   return (
     <div id="page-game">
-      <p>Olá, {auth.name}</p>
+      <Unity
+        unityContext={unityContext}
+        style={{ width: "100vw", height: "100vh" }}
+      />
       <button id="corner-button" onClick={logout}>
         <i className="logout-icon" />
       </button>
