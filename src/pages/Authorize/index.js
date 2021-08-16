a0ba21ca80fa9cb233c172611891ce30c862818b
Arnor Neto <arnor.neto@lavid.ufpb.br>
2021-08-16 16:46:36 -0300

    authorize request optimization

--- src/pages/Authorize/index.js
+++ src/pages/Authorize/index.js
@@ -6,6 +6,7 @@ import { useHistory } from "react-router-dom";
 import Loading from "../../assets/loading.svg";
 import { storeAuth } from "../../store/modules/auth/actions";
 import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const Authorize = () => {
   const dispatch = useDispatch();
@@ -21,46 +22,48 @@ const Authorize = () => {
   };
 
   //libraske API authentication request
-  axios
-    .post(`${process.env.REACT_APP_API_URL}/libraske/auth`, authData)
-    .then((response) => {
-      console.log(response.data);
+  useEffect(() => {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/libraske/auth`, authData)
+      .then((response) => {
+        console.log(response.data);
 
-      let data = {
-        name: response.data.name,
-        email: response.data.email,
-        cpf: response.data.cpf,
-        access_token: response.data.accessToken,
-        refresh_token: response.data.refreshToken,
-      };
+        let data = {
+          name: response.data.name,
+          email: response.data.email,
+          cpf: response.data.cpf,
+          access_token: response.data.accessToken,
+          refresh_token: response.data.refreshToken,
+        };
 
-      dispatch(storeAuth(data));
-      history.push("/play");
-    })
-    .catch((err) => {
-      try {
-        toast.error(
-          <div>
-            {err.response.data}
-            <br />
-            <br /> Por favor, realize o login novamente.
-          </div>,
-          {
-            //onClose: () => history.push("/"),
-          }
-        );
-      } catch (error) {
-        toast.error(
-          <div>
-            Algo deu errado na autenticação. <br />
-            <br /> Por favor, realize o login novamente.
-          </div>,
-          {
-            //onClose: () => history.push("/"),
-          }
-        );
-      }
-    });
+        dispatch(storeAuth(data));
+        history.push("/play");
+      })
+      .catch((err) => {
+        try {
+          toast.error(
+            <div>
+              {err.response.data}
+              <br />
+              <br /> Por favor, realize o login novamente.
+            </div>,
+            {
+              //onClose: () => history.push("/"),
+            }
+          );
+        } catch (error) {
+          toast.error(
+            <div>
+              Algo deu errado na autenticação. <br />
+              <br /> Por favor, realize o login novamente.
+            </div>,
+            {
+              //onClose: () => history.push("/"),
+            }
+          );
+        }
+      });
+  });
 
   const fakeAuth = () => {
     let data = {
